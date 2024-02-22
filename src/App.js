0ac11099f3e84a09b72bb4496c78ddd0371f4b75
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-22 20:28:41 +0530

    adding stuff to landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='landing-container'>
+
+      <div className='landing-title-wrapper'>
+        <div className='landing-title'>
+
+        </div>
+      </div>
+
     </div>
   );
 }
