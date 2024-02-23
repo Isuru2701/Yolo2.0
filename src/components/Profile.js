bafe26eab614c518e8ed7c05c9fdb93fd6ac870c
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-23 20:41:57 +0530

    profile categories need to be added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,9 @@
 import React from "react";
 import '../styles/profile.css';
 import { Coffee } from "@mui/icons-material";
+import { Grid } from "@mui/material";
 export default function Checkout() {
+    const buttons = ['Button 1', 'Button 2', 'Button 3', 'Button 4', 'Button 5'];
     return (
         <div className="profile-container">
 
@@ -11,13 +13,36 @@ export default function Checkout() {
                 <hr />
 
                 <button className='profile-menu-button'>Stripe Portal</button>
-                <button className='kofi-button'><Coffee/> Buy us a coffee</button>
+                <button className='kofi-button'><Coffee /> Buy us a coffee</button>
             </div>
 
-            <div className='collections-container'>
-                
+            <Grid container spacing={3} color={'white'} justifyContent={'flex-start'}>
+                <Grid item xs={12} sm={6}>
+                    <div className="collection-item">
+                        <h1>Title</h1>
+                        <p>Collection Description</p>
+                        <button className='profile-menu-button'>Edit</button>
+                        <button className='profile-menu-button'>Delete</button>
+                    </div>
+                </Grid>
 
-            </div>
+                <Grid item xs={12} sm={6}>
+                    Collection 2
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    Collection 3
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    four
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    five
+                </Grid>
+            </Grid>
+
         </div>
     );
 }
