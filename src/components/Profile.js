b1e9ba13c8a539606809c0621487f430baea3320
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-23 20:25:19 +0530

    profile sidebar added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import '../styles/profile.css';
+import { Coffee } from "@mui/icons-material";
 export default function Checkout() {
     return (
-        <></>
+        <div className="profile-container">
+
+            <div className="aside">
+                <h1>Username</h1>
+                <p>email</p>
+                <hr />
+
+                <button className='profile-menu-button'>Stripe Portal</button>
+                <button className='kofi-button'><Coffee/> Buy us a coffee</button>
+            </div>
+
+            <div className='collections-container'>
+                
+
+            </div>
+        </div>
     );
 }
