8194dc036cfe40942e2462228ee1aa0be943595e
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-23 18:01:45 +0530

    added stylizing to checkout

--- src/components/payments/Checkout.js
+++ src/components/payments/Checkout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../styles/checkout.css';
 
 export default function Checkout() {
     return (
@@ -7,17 +8,24 @@ export default function Checkout() {
             <div className='item-info-container'>
                 <div className='item-info'>
                     <div classname='product-title'>
-
+                        <h1>TITLE</h1>
+                        <hr/>
                     </div>
                     <div classname='product-benefits'>
-
+                        <ul>
+                            <li>benefit</li>
+                            <li>benefit</li>
+                            <li>benefit</li>
+                            <li>benefit</li>
+                        </ul>
                     </div>
                 </div>
 
-
-
             </div>
             <div className='amt-info'>
+                <h1 className='amt'>$5</h1>
+                <hr style={{color: 'black'}}/>
+                <button className='checkout-btn'>Checkout With Stripe</button>
             </div>
 
         </div>
