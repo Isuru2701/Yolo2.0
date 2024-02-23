0a53c26458fb2581bafc1bf5283ba8e5a12e47a8
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-23 18:44:13 +0530

    success and cancel done

--- src/components/payments/Cancel.js
+++ src/components/payments/Cancel.js
@@ -1,7 +1,38 @@
-import React from "react";
-
-export default function Cancel() {
+import React from 'react';
+import Box from '@mui/material/Box';
+import { Cancel } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+function SuccessMessage() {
     return (
-        <></>
+        <div
+        style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            width: "100%",
+            backgroundColor: 'var(--base-grey)',
+            color: 'white'
+            
+        }}
+        >
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+                height="100vh"
+            >
+                <Cancel style={{ fontSize: 60, color: 'red' }} />
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Canceled!
+                </Typography>
+                <Typography variant="subtitle1" component="p">
+                    The operation was canceled. You were not charged.
+                </Typography>
+            </Box>
+        </div>
     );
 }
+
+export default SuccessMessage;
--- src/components/payments/Success.js
+++ src/components/payments/Success.js
@@ -1,7 +1,38 @@
-import React from "react";
-
-export default function Success() {
+import React from 'react';
+import Box from '@mui/material/Box';
+import { Check } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+function SuccessMessage() {
     return (
-        <></>
+        <div
+        style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            width: "100%",
+            backgroundColor: 'var(--base-grey)',
+            color: 'white'
+            
+        }}
+        >
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
+                height="100vh"
+            >
+                <Check style={{ fontSize: 60, color: 'green' }} />
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Success!
+                </Typography>
+                <Typography variant="subtitle1" component="p">
+                    Your operation was successful.
+                </Typography>
+            </Box>
+        </div>
     );
 }
+
+export default SuccessMessage;
