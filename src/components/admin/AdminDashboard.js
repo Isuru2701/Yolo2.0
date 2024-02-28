c2c3af4d37dd8d5b9b352db096658d272df654ec
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-28 18:52:50 +0530

    structuring admin dashboard

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import '../'
 export default function AdminDashboard() {
     return (
-        <></>
+        <>
+            <div className='admin-dashboard-container'>
+                <div className='admin-header'>
+                    <h1>Admin Dashboard</h1>
+
+                </div>
+
+                <div className='admin-analytics'>
+                    <button>generate report</button>
+                </div>
+
+                <div className='admin-actions'>
+                    <button>
+                        view boost requests
+                    </button>
+                    <button>
+                        create a collection
+                    </button>
+                    <button>
+                        edit a collection
+                    </button>
+
+                </div>
+
+
+            </div>
+        </>
     );
 }
