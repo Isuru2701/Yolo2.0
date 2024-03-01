3559aa0ab9b17387e421c506dd564f3aa443ed6b
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-01 06:51:19 +0530

    env change and login change

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,44 @@
+
 import React, { useState } from "react";
-import '../styles/user.css'
+import '../styles/user.css';
 
 export default function Login() {
-    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
 
-    const handleLogin = (event) => {
+    const handleLogin = async (event) => {
         event.preventDefault(); // Prevent the form from refreshing the page
 
-        // Here you can handle the login logic (e.g., send the username and password to your server)
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({"email": email,"password": password })
+            });
 
-
-        console.log(`Logging in with username: ${username} and password: ${password}`);
+            if (response.ok) {
+                // Update cookie or perform any other successful login action
+            } else {
+                setError(true);
+            }
+        } catch (error) {
+            setError(true);
+        }
     };
 
     return (
         <>
             <div className="registration-container">
-
                 <div className="login-section">
                     <h2 className="registration-heading">Login</h2>
                 </div>
                 <form className="registration-form" onSubmit={handleLogin}>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" required onChange={(e) => setUsername(e.target.value)} />
+                        <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
@@ -34,8 +48,9 @@ export default function Login() {
                     <br></br>
                     <a className="redirect" href="/register">New to the platform? create an account for free.</a>
                 </form>
-
+                {error && <p>Error occurred during login.</p>}
             </div>
         </>
     );
 }
+
