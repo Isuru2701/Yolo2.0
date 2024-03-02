74d17dada10920a0c4ac5a37af94a0c49127099b
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-02 05:57:58 +0530

    cc verify modal done

--- src/components/elements/CCVerify.js
+++ src/components/elements/CCVerify.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Modal } from '@mui/material';
-import {Typography} from '@mui/material';
-import {Popover} from '@mui/material';
+import { Typography } from '@mui/material';
+import { Popover } from '@mui/material';
+import { Button } from '@mui/material';
+
+import '../../styles/creators.css'
 export default function CCVerify() {
 
   return (
@@ -9,18 +12,24 @@ export default function CCVerify() {
       <Popover
         open={true}
         anchorOrigin={{
-          vertical: 'center',
+          vertical: 'top',
           horizontal: 'center',
         }}
         transformOrigin={{
           vertical: 'center',
           horizontal: 'center',
         }}
+        style={{ backgroundColor: 'rgba(0,0,0,0.6)', width: "100%", height: '100%', color: "red" }}
 
       >
-        <Typography sx={{ p: 10 }}> Are you a content creator? </Typography>
-        <Typography sx={{ p: 2 }}> </Typography>
+        <Typography sx={{ p: 10, fontSize: 20, margin: 5, color: "var(--base-grey)" }}> Are you a content creator?
+          Enroll in our creators program to make a bigger audience!</Typography>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button variant="contained" color="primary" style={{ margin: 10 }} onClick={() => window.location.href = '/creators/enroll'}>Enroll</Button>
+          <Button variant="contained" color="secondary" style={{ margin: 10 }} onClick={() => window.location.href = '/'}>No, Thanks</Button>
+        </div>
       </Popover>
+
     </>
   );
 
