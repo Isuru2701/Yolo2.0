377eaeafdbdcce6fea16bc5fefdaf79f4f43d8b7
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-04 08:47:39 +0530

    stylized landing UI, still need more

--- src/App.js
+++ src/App.js
@@ -2,31 +2,41 @@ import './App.css';
 
 function App() {
   return (
-      <div className='landing-container'>
+    <div className='landing-container'>
 
       <div className='landing-title'>
-        <h1>Hello <br /> There!</h1>
+        <div>
+          <h1>Hello <br /> There!</h1>
+          <div>
+            <img src='feature-icon-02.svg' width={"200%"} style={{marginLeft: "100%"}} />
+          </div>
+        </div>
+        <br/>
+        <h6 style={{ color: 'white', textAlign: 'center' }}> Ready to Dive into a World of Endless Entertainment? Let's Get Started!</h6>
+        <br />
+        
+        <p style={{border: 'none', backgroundColor: 'var(--neutral-purple)', borderRadius: 20, padding: 10}}>YOLO is a media recommendation system that left you find your next obsession, like your asking a friend!</p>
       </div>
-{/* 
+      {/* 
       <div className='prompts'> */}
-        <center><button id="prompt"> Get prompt </button></center>
+      <center><button id="prompt"> Get prompt </button></center>
       {/* </div> */}
 
-    <div className='text_area'>
+      <div className='text_area'>
         <h2 id='write'>Write your prompt here:</h2>
         <br />
         <textarea rows="4" cols="50"></textarea>
 
-      <div className='buttons'>
+        <div className='buttons'>
 
-        <button id="obscure">obscure</button>
-        <button id="limit-to">Limit to</button>
-        <button id="Submit">Submit</button>
+          <button id="obscure">obscure</button>
+          <button id="limit-to">Limit to</button>
+          <button id="Submit">Submit</button>
+
+        </div>
 
       </div>
-
-      </div>
-      </div>
+    </div>
   );
 }
 
