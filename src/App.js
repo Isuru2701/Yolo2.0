75efbbfd3daf757b1edd01ef2c51e20345cd4a5f
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-04 15:20:19 +0530

    app fetching media

--- src/App.js
+++ src/App.js
@@ -1,8 +1,263 @@
 import './App.css';
 import { Select, MenuItem } from '@mui/material';
-import {InputLabel} from '@mui/material';
+import { InputLabel } from '@mui/material';
+import { useState } from 'react';
+
 
 function App() {
+  const [error, setErorr] = useState('');
+  const [text, setText] = useState('');
+  const [limit, setLimit] = useState('');
+  const [media, setMedia] = useState({ 'url': '', 'title': '', 'type': '', 'premium': false })
+  const handleFetchKeywords = async (event) => {
+    event.preventDefault();
+    console.log('fetching keywords');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/keywords`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "prompt": text })
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+        //stringify the list
+        let string = data.join(', ');
+
+        //check limit to is set
+        if(limit === ''){
+          handleFetchSongs(string);
+          handleFetchMovies(string);
+          handleFetchTV(string);
+          handleFetchBooks(string);
+          handleFetchAnime(string);
+
+        }
+
+        else if (limit === 'movies') {
+          handleFetchMovies(string);
+          
+        }
+        else if (limit === 'tv') {
+          handleFetchTV(string);
+          
+        }
+        else if (limit === 'songs') {
+          handleFetchSongs(string);
+          
+        }
+        else if (limit === 'anime') {
+          handleFetchAnime(string);
+          handleFetchAnimeMovie(string);
+          
+        }
+        else if (limit === 'books') {
+          handleFetchBooks(string);
+          
+        }
+
+
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+
+  }
+
+  // example request : http://localhost:5000/movies?keywords=marvel,adventure&media_type=movie (movie / tv)
+
+  const handleFetchSongs = async (keywords) => {
+    console.log('fetching songs');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/songs?${keywords}&media_type=song`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+  }
+
+  const handleFetchMovies = async (keywords) => {
+    console.log('fetching songs');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/movies?${keywords}&media_type=movie`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+  }
+
+  const handleFetchTV = async (keywords) => {
+    console.log('fetching songs');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/tv?${keywords}&media_type=tv`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+  }
+
+  const handleFetchBooks = async (keywords) => {
+    console.log('fetching songs');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/books${keywords}&media_type=song`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+  }
+
+
+  const handleFetchAnime = async (keywords) => {
+    console.log('fetching songs');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/anime?${keywords}&media_type=tv`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+  }
+
+  const handleFetchAnimeMovie = async (keywords) => {
+    console.log('fetching songs');
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/anime?${keywords}&media_type=movie`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (response.ok) {
+        //print response contents
+        var data = response.json();
+        console.log(response.json());
+
+
+
+      } else {
+        setErorr("Sorry, we didn't quite catch that. try");
+
+      }
+    } catch (error) {
+    }
+
+  }
+
+
+
+
   return (
     <div className='landing-container'>
 
@@ -31,8 +286,8 @@ function App() {
       <br />
       <div className='text_area' id='chat' style={{ zIndex: 20 }}>
         <br />
-        <h1 style= {{textAlign:'left', color: 'white'}}>Whats on your mind?</h1>
-        <textarea rows="4" cols="50"></textarea>
+        <h1 style={{ textAlign: 'left', color: 'white' }}>Whats on your mind?</h1>
+        <textarea rows="4" cols="50" onChange={(e) => setText(e.target.value)}></textarea>
 
         <div className='buttons'>
           <InputLabel id="demo-simple-select-label" sx={{
@@ -42,21 +297,21 @@ function App() {
             labelId="demo-simple-select-label"
             label='Limit'
             className="landing-button"
-            sx = {{
+            sx={{
               color: 'var(--tone-highlight)',
               padding: '5px'
             }}
-          
-          >
-            <MenuItem value="None" className="menu-item">None</MenuItem>
-            <MenuItem value="Movies" className="menu-item">Movies</MenuItem>
-            <MenuItem value="TV"className="menu-item">TV</MenuItem>
-            <MenuItem value="Anime" className="menu-item">Anime</MenuItem>
-            <MenuItem value="Songs" className="menu-item">Songs</MenuItem>
-            <MenuItem value="Books" className="menu-item">Books</MenuItem>
-          </Select>
-          <button className="landing-button submit">Submit</button>
 
+          >
+            <MenuItem value="None" className="menu-item"  onChange={() => setLimit('')}>None</MenuItem>
+            <MenuItem value="Movies" className="menu-item" onChange={() => setLimit('movies')}>Movies</MenuItem>
+            <MenuItem value="TV" className="menu-item" onChange={() => setLimit('tv')}>TV</MenuItem>
+            <MenuItem value="Anime" className="menu-item" onChange={() => setLimit('anime')}>Anime</MenuItem>
+            <MenuItem value="Songs" className="menu-item" onChange={() => setLimit('songs')}>Songs</MenuItem>
+            <MenuItem value="Books" className="menu-item" onChange={() => setLimit('books')}>Books</MenuItem>
+          </Select>
+          <button className="landing-button submit" onClick={handleFetchKeywords}>Submit</button>
+          {error && <p className='error-message'>{error}</p>}
         </div>
 
       </div>
