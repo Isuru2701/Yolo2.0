9c2cb4d894d90ac1228faee54855a93e62c81ad0
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-13 10:18:10 +0530

    approve and reject complete

--- src/components/admin/ApproveRequests.js
+++ src/components/admin/ApproveRequests.js
@@ -38,7 +38,7 @@ export default function ApproveRequests() {
     }
 
     //approve a request 
-    const handleApprove = async (docId) => {
+    const handleApprove = async (docId, index) => {
         try {
             const response = await fetch(`${process.env.REACT_APP_API_URL}/creators/approve?doc=${docId}`, {
                 method: 'GET',
@@ -49,8 +49,15 @@ export default function ApproveRequests() {
 
             if (response.ok) {
                 // Successfully approved the boost request
-                // Optionally, you can update the UI to reflect the approval
+
                 console.log('Boost request approved successfully');
+
+                setRequests(prevRequests => {
+                    const updatedRequests = [...prevRequests];
+                    updatedRequests[index].status = "approved";
+                    return updatedRequests;
+                });
+
             } else {
                 // Handle error response from the backend
                 console.error('Failed to approve boost request');
@@ -61,7 +68,7 @@ export default function ApproveRequests() {
         }
     }
     //reject a request
-    const handleReject = async (docId) => {
+    const handleReject = async (docId, index) => {
         try {
             const response = await fetch(`${process.env.REACT_APP_API_URL}/creators/reject?doc=${docId}`, {
                 method: 'GET',
@@ -72,7 +79,12 @@ export default function ApproveRequests() {
 
             if (response.ok) {
                 // Successfully rejected the boost request
-                // Optionally, you can update the UI to reflect the rejection
+
+                setRequests(prevRequests => {
+                    const updatedRequests = [...prevRequests];
+                    updatedRequests[index].status = "rejected";
+                    return updatedRequests;
+                });
                 console.log('Boost request rejected successfully');
             } else {
                 // Handle error response from the backend
@@ -115,16 +127,22 @@ export default function ApproveRequests() {
             <button onClick={generateReport}>Generate report</button>
             <div className="requests-grid">
 
-                {requests.map((request) => (
+                {requests.map((request, index) => (
                     <div key={request.id} className="request">
                         <h1>{request.title}</h1>
                         {request.email}
                         <p><a href={request.content_url}>Link</a></p>
-
-                        <button onClick={() => handleApprove(request.doc_id)}>Approve</button>
-                        <button className='reject' onClick={() => handleReject(request.doc_id)}>Reject</button>
+                        {request.status === "pending" ? (
+                            <>
+                                <button onClick={() => handleApprove(request.doc_id, index)}>Approve</button>
+                                <button className='reject' onClick={() => handleReject(request.doc_id, index)}>Reject</button>
+                            </>
+                        ) : (
+                            <div>{request.status === "approved" ? "Approved" : "Rejected"}</div>
+                        )}
                     </div>
                 ))}
+                
             </div>
         </div>
     );
