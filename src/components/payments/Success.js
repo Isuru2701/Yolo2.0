99222dc9a2f5ba89824e2eeb69901e9449fb5648
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-03-13 12:54:52 +0530

    success works

--- src/components/payments/Success.js
+++ src/components/payments/Success.js
@@ -11,51 +11,78 @@ function SuccessMessage() {
     const [error, setError] = useState("");
 
     const handleLedger = async () => {
-        
+
 
         console.log(Cookies.get("email"));
         //hit up /upgrade with the email
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/users/upgrade?email=${Cookies.get('email')}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            });
+        if (Cookies.get('boost_doc')) {
+            const boost_doc = Cookies.get('boost_doc');
+            console.log(boost_doc);
+            try {
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/creators/approve_payment?doc=${boost_doc}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
 
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);     
-                
-                //update the cookie
-                Cookies.set('premium', true);
-                
+                if (response.ok) {
+                    // Successfully approved the boost request
+                    console.log('Boost request approved successfully');
+                } else {
+                    console.log('Failed to approve the boost request');
+                }
 
-            } else {
-                setError("failed to upgrade");
+                Cookies.remove('boost_doc');
+            } catch (error) {
+                console.error('Error:', error);
             }
+
         }
-        catch(error) {
-            console.log(error);
-        }
-            
-    };
+        else {
+            try {
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/users/upgrade?email=${Cookies.get('email')}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data);
+
+                    //update the cookie
+                    Cookies.set('premium', true);
+
+
+                } else {
+                    setError("failed to upgrade");
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+
+        };
+    }
+
 
     handleLedger();
 
 
     return (
         <div
-        style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            width: "100%",
-            backgroundColor: 'var(--base-grey)',
-            color: 'white'
-            
-        }}
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                width: "100%",
+                backgroundColor: 'var(--base-grey)',
+                color: 'white'
+
+            }}
         >
             <Box
                 display="flex"
